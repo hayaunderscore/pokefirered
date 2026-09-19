18f8b103621ffb0c1b6ee5fb534ec4fe7930e3d5
hayaunderscore <stophayapls123@proton.me>
2026-09-19 10:30:46 +0800

    here we are

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -197,7 +197,7 @@
 #define FLAG_HIDE_MOLLY_IN_DEEP_RUINS_FINAL_HALLWAY        0x0B3
 #define FLAG_HIDE_MOLLY_IN_MT_SILVER_PEAK                  0x0B4
 #define FLAG_HIDE_BRET_IN_MT_SILVER_PEAK                   0x0B5
-#define FLAG_0x0B6               0x0B6
+#define FLAG_HIDE_RESEARCHERS_IN_LAB_RESEARCH_ROOM         0x0B6
 #define FLAG_0x0B7               0x0B7
 #define FLAG_0x0B8               0x0B8
 #define FLAG_0x0B9               0x0B9
--- src/event_object_movement.c
+++ src/event_object_movement.c
@@ -465,6 +465,7 @@ static const u8 gInitialMovementTypeFacingDirections[MOVEMENT_TYPES_COUNT] = {
 #define OBJ_EVENT_PAL_TAG_SERENA                      0x1123
 #define OBJ_EVENT_PAL_TAG_BRET                        0x1124
 #define OBJ_EVENT_PAL_TAG_NPC                         0x1125
+#define OBJ_EVENT_PAL_TAG_AMBER                       0x1126
 #define OBJ_EVENT_PAL_TAG_NONE                        0x11FF
 
 #include "data/object_events/object_event_graphics_info_pointers.h"
@@ -504,6 +505,7 @@ static const struct SpritePalette sObjectEventSpritePalettes[] = {
 	{gObjectEventPal_Serena,                  OBJ_EVENT_PAL_TAG_SERENA},
 	{gObjectEventPal_Bret, 					  OBJ_EVENT_PAL_TAG_BRET},
 	{gObjectEventPal_Player,                  OBJ_EVENT_PAL_TAG_NPC},
+	{gObjectEventPal_Amber,                   OBJ_EVENT_PAL_TAG_AMBER},
     {NULL,                                    OBJ_EVENT_PAL_TAG_NONE},
 };
 
